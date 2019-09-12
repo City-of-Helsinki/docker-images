9c340e86a6ca010f62d64ce7e5f8c4bf56674ac1
Frank Wickström <frank.wickstrom@anders.fi>
2019-09-12 16:49:14 +0300

    Add optional -u command for installing as user

--- templates/tools/pip-install.sh
+++ templates/tools/pip-install.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
-usage() { echo "Usage: $0 [-p <path>] -c <command>" 1>&2; exit 1; }
 
-while getopts ":p::c:" o; do
+function usage {
+    echo "Usage: $0 [-p <path>] [-u <user>] -c <command>"
+    echo "  -c      command to run"
+    echo "  -p      path to run in"
+    echo "  -u      user to run as (forces pip --user flag)"1>&2
+    exit 1;
+}
+
+while getopts ":p::c:u::" o; do
     case "${o}" in
         p)
             p=${OPTARG}
             ;;
         c)
             c=${OPTARG}
+            ;;
+        u)
+            u=${OPTARG}
             ;;
         *)
             usage
@@ -25,5 +35,14 @@ if [ ! -z "${p}" ]; then
     path_command="cd ${p} &&"
 fi
 
-su - appuser -c "${path_command} pip install --user --no-cache-dir ${c}"
+
+if [ ! -z "${u}" ]; then
+    echo "Running with user"
+    su - appuser -c "${path_command} pip install --user --no-cache-dir ${c}"
+else
+    echo "Running without user"
+    bash -c "${path_command} pip install --no-cache-dir ${c}"
+fi
+
+
 
