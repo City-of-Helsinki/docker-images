b32cc531b81af9ae3233b32cc995cc54f8aff78c
Frank Wickström <frank.wickstrom@anders.fi>
2019-09-16 08:38:25 +0300

    Add PostGIS images

---
+++ templates/postgis/update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Derived from https://github.com/docker-library/postgres/blob/master/update.sh
+set -x
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */Dockerfile )
+fi
+versions=( "${versions[@]%/Dockerfile}" )
+for version in "${versions[@]}"; do
+	IFS=- read pg_major postgis_major <<< "$version"
+	echo $postgis_major
+	echo $pg_major
+    git ls-remote --tags https://github.com/postgis/postgis.git "refs/tags/$postgis_major.*"
+    test="$(git ls-remote --tags https://github.com/postgis/postgis.git 'refs/tags/$postgis_major.*')"
+    echo $test
+
+    fullVersion="$(git ls-remote --tags https://github.com/postgis/postgis.git "refs/tags/$postgis_major.*" | tail -n1 | sed 's%.*refs/tags/%%')"
+
+	[ -z "$fullVersion" ] && { echo >&2 "Unable to find package for PostGIS $postgis_major on Postgres $pg_major"; exit 1; }
+
+	srcVersion="${fullVersion%%+*}"
+	srcSha256="$(curl -sSL "https://github.com/postgis/postgis/archive/$srcVersion.tar.gz" | sha256sum | awk '{ print $1 }')"
+
+    set -x
+    cp Dockerfile.alpine.template initdb-postgis.sh update-postgis.sh "$version/$variant/"
+    mv "$version/$variant/Dockerfile.alpine.template" "$version/$variant/Dockerfile"
+    sed -i 's/%%PG_MAJOR%%/'"$pg_major"'/g; s/%%POSTGIS_VERSION%%/'"$srcVersion"'/g; s/%%POSTGIS_SHA256%%/'"$srcSha256"'/g' "$version/$variant/Dockerfile"
+done
